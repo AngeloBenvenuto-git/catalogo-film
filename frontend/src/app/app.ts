f8da93af7c31e1b57321b71c7683999a9fa85357
Paky <pasquale.rizzo223gmail.com>
2026-03-14 19:54:32 +0100

    aggiornamento

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
-import { FilmList } from './components/film-list/film-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, FilmList],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
